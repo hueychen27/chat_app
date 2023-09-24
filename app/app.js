4fabb6eacbacc5c092a65bb0c1b8e01038977b31
Da_Person <hueychen27@outlook.com>
2023-09-24 14:57:15 -0700

    Add files

---
+++ app/app.js
@@ -0,0 +1,55 @@
+const socket = io('ws://localhost:3500');
+
+function waitForVariableValue() {
+    return new Promise((resolve) => {
+        const interval = setInterval(() => {
+            if (document.visibilityState === "visible") {
+                clearInterval(interval);
+                resolve();
+            }
+        }, 10);
+    });
+}
+
+var targetObj = {};
+var targetProxy = new Proxy(targetObj, {
+    set: async function (target, key, value) {
+        if (typeof document.visibilityState !== "undefined" && document.visibilityState === "hidden") await waitForVariableValue();
+        document.dispatchEvent(new Event("scrollDown"));
+        target[key] = value;
+        return true;
+    }
+});
+
+function sendMessage(e) {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    if (input.value) {
+        socket.emit('message', input.value);
+        input.value = "";
+    }
+    input.focus();
+}
+
+document.querySelector('form').addEventListener('submit', sendMessage);
+
+socket.addEventListener("open", (e) => {
+    socket.send("Server opened...");
+})
+
+socket.on("message", (data) => {
+    const li = document.createElement("li");
+    li.textContent = data.type === "message" ? `${data.shortId}: ${data.text}` : data.type === "disconnect" ? `User ${data.shortId} disconnected.` : "";
+    li.title = `id: ${data.id}`;
+    document.querySelector("ul").appendChild(li);
+    targetProxy.scroll = 1;
+})
+
+document.addEventListener("scrollDown", () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: "smooth"
+    });
+    targetProxy.scroll = 0;
+})
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import { createServer } from "http"
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
+    }
+})
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+
+    socket.on('message', data => {
+        console.log(data)
+        io.emit('message', { id: socket.id, shortId: socket.id.substring(0, 5), text: data, type: "message" });
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', { id: socket.id, shortId: socket.id.substring(0, 5), type: "disconnect" });
+        console.log(`User ${socket.id} disconnected`)
+    })
+})
+
+httpServer.listen(3500, () => console.log('Listening on port 3500'))
